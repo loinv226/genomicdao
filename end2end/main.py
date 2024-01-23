6beb885fd630b80fd52274350386dc7d866122d2
dev <dev@mac.local>
2024-01-23 18:59:37 +0700

    update: add base components

---
+++ end2end/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+def main():
+    print("Hello, this is a simulation of the process of analyzing your gene data that has been saved in the file sample.txt")
+    print("Loading...")
+    sleep(1)
+    print("Uploading data...")
+
+if __name__ == "__main__":
+    main()
---
+++ end2end/src/client.py
@@ -0,0 +1,24 @@
+import requests
+
+def check_session_exist():
+        pass
+    
+def generate_new_account_and_session():
+        pass
+    
+def select_gene_file():
+      pass
+
+def check_default_gene_file_exist():
+        pass
+
+def submit_gene_data(file):
+    pass
+
+def show_gene_info():
+    pass
+
+def show_reward_token():
+    pass
+    
+
