d6c9c24158b567cde8c49fe7b5cb47be2b216622
dev <dev@mac.local>
2024-01-25 01:47:08 +0700

    update(contract): change version hardhat, add more package

--- genomicdao/hardhat.config.js
+++ genomicdao/hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+const { task } = require("hardhat/config");
+require("dotenv").config();
+
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    default: {
+      url: process.env.CHAIN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined
+          ? process.env.PRIVATE_KEY.split(",")
+          : [],
+      chainId: process.env.CHAIN_ID,
+    },
+  },
 };
